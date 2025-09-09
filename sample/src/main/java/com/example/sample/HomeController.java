3a86a4613a393f687229631820bd46fe88edacfc
Anushya RajaLakshmi <anushyamaharaja158@gmail.com>
2025-09-09 15:42:05 +0530

    Initial commit

---
+++ sample/src/main/java/com/example/sample/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/greet")
+    public String greeting(){
+        return "hello , this is jenkins sample program";
+    }
+}
